cc0709e8b0db2a50eb981388993c50b42a4e713e
sausageRoll <drozdovigor1996@gmail.com>
2017-07-05 15:06:59 +0300

    template

---
+++ src/main/java/com/epam/testsystem/github/service/HttpResolverService.java
@@ -0,0 +1,17 @@
+package com.epam.testsystem.github.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * github_test
+ * Created on 05.07.17.
+ */
+
+@Service
+public class HttpResolverService {
+
+    public String get(final String url) {
+        // TODO: 05.07.17 return String from url
+        return null;
+    }
+}
