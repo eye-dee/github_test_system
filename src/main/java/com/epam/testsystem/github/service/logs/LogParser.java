8d7faeccc7ed8f1cf499661776574acdfb2e3b8e
MortyMerr <anton.nazarof@mail.ru>
2017-07-13 12:36:59 +0300

    Useless delete

--- src/main/java/com/epam/testsystem/github/service/logs/LogParser.java
+++ src/main/java/com/epam/testsystem/github/service/logs/LogParser.java
@@ -1,14 +1,9 @@
 package com.epam.testsystem.github.service.logs;
 
 import com.epam.testsystem.github.model.GradleLog;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Created by antonnazarov on 12.07.17.
@@ -16,22 +11,6 @@ import java.util.regex.Pattern;
 
 @Service
 public class LogParser {
-    private static final Logger LOGGER = LoggerFactory.getLogger(LogParser.class);
-
-    private List<String> getLineWith(final String log, final String word) {
-        Matcher m = Pattern.compile(String.format("(?m)%s.*", word)).matcher(log);
-        final List<String> founded = new ArrayList<>();
-        while (m.find()) {
-            founded.add(m.group(0));
-        }
-        return founded;
-    }
-
-    private String getCycleInf(final String log, final String left, final String right) {
-        final int leftPos = log.indexOf(left);
-        return log.substring(leftPos, log.indexOf(right, leftPos));
-    }
-
     public GradleLog getStructedLog(final String log) {
         String[] splitted = log.split("\n");
         GradleLog structedLog = new GradleLog();
