583ca1c27dc66b2a6a99b035c398b3d5db8ab45e
sausageRoll <drozdovigor1996@gmail.com>
2017-07-14 12:36:46 +0300

    ui improve

--- src/main/resources/static/js/table.js
+++ src/main/resources/static/js/table.js
@@ -12,7 +12,8 @@ $("#getAll").click(() => {
                             email: result[i].email,
                             git: result[i].githubNick,
                             time: null,
-                            successful: null
+                            successful: null,
+                            log: null
                         }
                     )
                 }
@@ -21,7 +22,8 @@ $("#getAll").click(() => {
                         email: result[i].email,
                         git: result[i].githubNick,
                         time: result[i].tasks[j].startTime,
-                        successful: result[i].tasks[j].successful
+                        successful: result[i].tasks[j].successful,
+                        log: result[i].tasks[j].log
                     };
                     tasks.push(task);
                 }
@@ -35,14 +37,25 @@ $("#getAll").click(() => {
     })
 });
 
+var printIcon = function (cell, formatterParams) { //plain text value
+    return "<i>penis</i>";
+};
 
 function setTable(data) {
     $("#example-table").tabulator({
+        rowClick: function (e, id, dataa, row) {
+            $("#log").text(JSON.stringify(dataa.log, null, ' '));
+        },
         fitColumns: true,
         movableCols: true,
         movableRows: true,
         tooltips: true,
         columns: [
+            {
+                formatter: printIcon, width: 40, align: "center", cellClick: function (e, cell) {
+                alert("Printing row data for")
+            }
+            },
             {title: "Email", field: "email", sorter: "string"},
             {title: "Git", field: "git", sorter: "string"},
             {title: "Start time", field: "time", width: 130, sorter: "date", align: "center"},
