7618b46f277b81ca245be0b6edd9f88f815686e9
sausageRoll <drozdovigor1996@gmail.com>
2017-07-14 14:30:44 +0300

    LogResolver interface refactor

--- src/main/java/com/epam/testsystem/github/service/log/LogResolver.java
+++ src/main/java/com/epam/testsystem/github/service/log/LogResolver.java
@@ -5,5 +5,10 @@ package com.epam.testsystem.github.service.log;
  * Create on 12.07.17.
  */
 public interface LogResolver {
+    default String getLogs(final long buildId) {
+        return getLogs(null, buildId);
+    }
+
     String getLogs(String user, final long buildId);
+
 }
